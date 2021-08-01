d461fca94e653aed4c2fb63088299a226e598e42
liniker <linikergabrieldasilva@gmail.com>
2021-08-01 14:09:38 -0300

    H2 database

---
+++ src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
