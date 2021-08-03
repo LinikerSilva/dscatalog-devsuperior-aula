92ba8b58a3f5a521855243e70c8c065ec1cc023d
liniker <linikergabrieldasilva@gmail.com>
2021-08-02 22:43:12 -0300

    Database seeding

--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import com.devsuperior.dscatalog.entities.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
@@ -14,7 +15,10 @@ public class CategoryService {
   @Autowired
   private CategoryRepository repository;
 
+  @Transactional(readOnly = true)
   public List<Category> findAll() {
     return repository.findAll();
   }
 }
+
+
